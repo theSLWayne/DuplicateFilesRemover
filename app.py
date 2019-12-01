cc8ffb4b4db70c4d380a85cee83db147343cb363
theSLWayne <hiranhasanka@gmail.com>
2019-12-01 12:41:11 +0530

    Invalid folder path alert

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 from flask import Flask 
-from flask import render_template, url_for, request, flash
+from flask import render_template, url_for, request, flash, redirect, json, session
 from dr import duplicates, isDirValid, deleteFiles
 
 app = Flask(__name__)
+app.secret_key = "b'\x0c\x83\xb9jL\x87\xc9\x9e\x16\xbe\xa2.\xae&\xb6\x1a\xb8\x05E\xd1\xea\x83\x8a\xdf'"
 
 dirPath = ''
 
@@ -12,6 +13,17 @@ def Home():
 
 @app.route('/', methods=['POST'])
 def Home_post():
-    dirPath = request.form['text']
+    dirPath = request.form['folderpath']
+    dPath = json.dumps(dirPath)
+    session['dPath'] = dPath
     if isDirValid(dirPath):
-        flash("Valid Folder Path!")
+        return redirect(url_for('DupRem', dPath=dPath))
+    else:
+        flash('The folder path is invalid.', 'error')
+        return redirect(url_for('Home'))
+
+@app.route('/duprem')
+def DupRem():
+    dPath = request.args['dPath']
+    dPath = session['dPath']
+    return 'OK'
